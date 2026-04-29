998d7f8ebcedc354b97cf191db552d972e1fabca
brenonlps <brenolopes.xy@gmail.com>
2026-04-29 02:06:50 -0300

    feat: add Footer component with copyright notice

---
+++ app/components/(public)/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full h-30 bg-gray-800 text-white flex items-center justify-center">
+            <p className="text-sm">&copy; 2024 Festae. Todos os direitos reservados.</p>
+        </footer>
+    )
+}
