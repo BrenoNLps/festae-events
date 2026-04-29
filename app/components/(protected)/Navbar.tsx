ae5af62c4ab5f7bea05b2c956e4a047238afdb7a
brenonlps <brenolopes.xy@gmail.com>
2026-04-29 08:09:08 -0300

    feat: add Navbar component for user navigation and authentication

---
+++ app/components/(protected)/Navbar.tsx
@@ -0,0 +1,29 @@
+'use client'
+import {LogOut, User } from 'lucide-react'
+import { createClient } from '../../lib/supabase/client'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+
+export default function Navbar() {
+const supabase = createClient()
+const router = useRouter()
+
+async function handleLogout() {
+    await supabase.auth.signOut()
+    router.push('/')
+}
+
+return (
+        <nav className="h-16 w-full  border-b border-gray-300 flex items-center justify-between px-8">
+        <span className="text-xl font-bold text-purple-600">🌐 Festaê</span>
+        <div className="flex items-center gap-3">
+            <Link href={"/profile"} className="w-9 h-9 rounded-full bg-gray-800 flex items-center justify-center text-white hover:bg-gray-700 transition">
+                <User className="h-5 w-5" />
+            </Link>
+            <button onClick={handleLogout}className="flex items-center gap-2 text-sm text-gray-500 hover:text-red-500 transition">
+                <LogOut className="h-5 w-5" />
+            </button>
+        </div>
+        </nav>
+    )
+}
