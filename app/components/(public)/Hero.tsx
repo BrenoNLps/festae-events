6a11b9f72aced88339dc3c471d4fbe13893102e0
brenonlps <brenolopes.xy@gmail.com>
2026-06-16 18:18:21 -0300

    feat(hero): imagens responsivas com picture element por breakpoint
    Substitui imagem única por variantes desktop/tablet/mobile em cada
    banner, usando <picture> com <source media> para carregar apenas a
    imagem do breakpoint atual.

--- app/components/(public)/Hero.tsx
+++ app/components/(public)/Hero.tsx
@@ -1,16 +1,31 @@
 'use client'
 import React from 'react'
-import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Parallax, Pagination, Navigation, Autoplay } from 'swiper/modules'
 import 'swiper/swiper-bundle.css'
 import './Hero.css'
 
-const slides=[
-    {image: '/images/hero/banner1.png',texto: 'A plataforma de eventos que conecta pessoas e cria experiências inesquecíveis.'},
-    {image: '/images/hero/banner2.png',texto: 'É se conectar com eventos incríveis e criar memórias inesquecíveis.'},
-    {image: '/images/hero/banner3.png',texto: 'Descubra, participe e compartilhe momentos únicos com a Festaê!'}    
+const slides = [
+    {
+        desktop: '/images/hero/banner1/desktop.png',
+        tablet:  '/images/hero/banner1/tablet.png',
+        mobile:  '/images/hero/banner1/mobile.png',
+        texto: 'A plataforma de eventos que conecta pessoas e cria experiências inesquecíveis.',
+    },
+    {
+        desktop: '/images/hero/banner2/desktop.png',
+        tablet:  '/images/hero/banner2/tablet.png',
+        mobile:  '/images/hero/banner2/mobile.png',
+        texto: 'É se conectar com eventos incríveis e criar memórias inesquecíveis.',
+    },
+    {
+        desktop: '/images/hero/banner3/desktop.png',
+        tablet:  '/images/hero/banner3/tablet.png',
+        mobile:  '/images/hero/banner3/mobile.png',
+        texto: 'Descubra, participe e compartilhe momentos únicos com a Festaê!',
+    },
 ]
+
 export default function Hero() {
     return (
         <>
@@ -24,9 +39,14 @@ export default function Hero() {
                 modules={[Parallax, Pagination, Navigation, Autoplay]}
                 className="mySwiper"
             >
-                {slides.map((item,index) =>(
+                {slides.map((item, index) => (
                     <SwiperSlide key={index}>
-                        <Image src={item.image} className="absolute inset-0 w-full h-full object-cover" width={800} height={600}  alt="Banner"/>
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <picture className="absolute inset-0 w-full h-full">
+                            <source media="(min-width: 1024px)" srcSet={item.desktop} />
+                            <source media="(min-width: 768px)"  srcSet={item.tablet} />
+                            <img src={item.mobile} alt="Banner" className="w-full h-full object-cover" />
+                        </picture>
                         <div className="title" data-swiper-parallax="-300">Festaê</div>
                         <div className="subtitle" data-swiper-parallax="-200">{item.texto}</div>
                     </SwiperSlide>
